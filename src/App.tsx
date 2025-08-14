50db0c5b25864bc9995c46e0673448cc417362f4
Azzam-coder-cpu <hafizazzam05@gmail.com>
2025-08-14 08:23:52 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Nav from './components/Navbar'
+import Hero from './components/Hero'
+import Feature from './components/Feature'
+import Faq from './components/Faq'
+import News from './components/Newsletter'
+import Footer from './components/Footer'
+function App() {
+   return <>
+   <Nav/>
+   <Hero/>
+   <Feature />
+   <Faq />
+   <News />
+   <Footer/>
+   </>
+}
+
+export default App
